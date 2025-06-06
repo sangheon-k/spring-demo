e283f054cc4629ed8a308c39e7f242a52b7cc10e
sangheon-k <sangheon.k@kakao.com>
2025-06-07 07:25:06 +0900

    MVC - practice

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
@@ -11,5 +12,11 @@ public class DemoController {
     public String demo(Model model){
         model.addAttribute("data", "demo!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
